cd88bcf61e97e8f9ecc9162d52043b911fdc3a3b
mkidushim <mkidushim@gmail.com>
2015-05-31 18:51:24 -0700

    Still working on json

--- TDL.js
+++ TDL.js
@@ -1,17 +1,25 @@
-	var title = $('#title').val();
-	var summary = $('#summary').val();
-	var date = $('#date').val();
+$(document).ready(function() {
+    var title = $('input#title').val();
+    var summary = $('input#summary').val();
+    var date = $('input#date').val();
+    $('body').on('click', '#add', function() {
+        console.log("button worked")
+        
+        $.ajax({
 
-	$(document).ready(function() {
-$('body').on('click', '#add', function(){
-	$.ajax({
-			method: 'POST'
-            url: 'get_todo_item_server.json',
-            dataType: 'json',
+            method: 'GET',
+            url: 'http://localhost/lf_projects/MySandbox%20/c4_TDL/get_todo_item.json',
+            dataType: 'JSON',
             cache: false,
+            crossDomain: true,
             success: function(response) {
                 console.log('success:', response)
+                $('h5#one').html(JSON.stringify(todo_items[0]))
             }
-        });
 
-	})
+        })
+
+
+
+    });
+});
